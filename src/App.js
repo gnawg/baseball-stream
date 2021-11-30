027d8fb7c117ca9399e78bc059fe4b8a5d669971
gnawg <gnawgy.gnawgy@gmail.com>
2021-11-29 19:16:22 -0500

    init

--- src/App.js
+++ src/App.js
@@ -1,24 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from "axios";
+import { useState } from "react";
+import "./App.css";
+
+const redirectURI = process.env.REDIRECT_URI || "http://localhost:3000";
+const clientID = "1jm2ajf9hva6pa6pz16zacwe8fa0ez";
+const scopes = "channel:read:redemptions channel:read:subscriptions";
+const twitchRoot = `https://api.twitch.tv/helix`;
 
 function App() {
+  const [subs, setSubs] = useState([]);
+  const [broadcasterID, setBroadcasterID] = "";
+
+  const getUser = async () => {
+    const data = await axios.get(`${twitchRoot}/users`, {
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+        "Client-Id": clientID,
+      },
+    });
+    console.log(data);
+  };
+
+  const getSubs = async () => {
+    const newSubs = await axios.get(
+      `${twitchRoot}/subscriptions/?broadcaster_id=${broadcasterID}`
+    );
+    setSubs([...subs, ...newSubs]);
+  };
+
+  const hash = document.location.hash
+    .slice(1)
+    .split("&")
+    .reduce((prev, curr) => {
+      const entry = curr.split("=");
+      return {
+        ...prev,
+        [entry[0]]: entry[1],
+      };
+    }, {});
+
+  console.log(hash);
+  const accessToken = hash["access_token"];
+
+  const me = accessToken ? getUser() : "";
+  console.log(me);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+    <body>
+      {accessToken ? (
+        `${JSON.stringify(me)}`
+      ) : (
         <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+          href={`https://id.twitch.tv/oauth2/authorize?client_id=${clientID}&redirect_uri=${redirectURI}&response_type=token&scope=${scopes}`}
         >
-          Learn React
+          login
         </a>
-      </header>
-    </div>
+      )}
+    </body>
   );
 }
 
